2c23e0a9710bc3a19316900309caf939daa7580a
RHcom007 <radjaherlangga2006@gmail.com>
2022-12-21 20:21:47 +0700

    Menambahkan 404 page dan memperbaiki keamanan

---
+++ 404/index.php
@@ -0,0 +1,40 @@
+<?php http_response_code(404) ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - Website tidak di temukan!</title>
+</head>
+    <style>
+        body {
+            color: white;
+            margin: 0 auto;
+            max-width: 800px;
+            padding: 0 20px;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background: #0e1e2f;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            flex-wrap: wrap;
+        }
+        h1,h3,h4{
+            margin: 0;
+        }
+        h1{
+            font-size: 250px;
+            color: rgb(61, 61, 61);
+        }
+        h3{
+            font-size: 30px;
+            margin-top: -170px;
+        }
+    </style>
+<body>
+    <h1>404</h1>
+    <h3>Halaman tidak ditemukan</h3>
+    <h4>Silahkan hubungi admin jika ini bukan kesalahan anda</h4>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+header('X-Frame-Options: deny');
 header("X-Content-Type-Options: nosniff");
-header("Content-Security-Policy: default-src 'self'; script-src ajax.googleapis.com; style-src 'self'; font-src 'self'; img-src 'self' ./image;");
+header("Content-Security-Policy: default-src 'self'; script-src 'sha256-+duGTPn+Tk4jtZ7Rjrp8CGafgOtZqzSQmFjzUpdG72Q=' 'sha256-6qcV7YJDmsywERNVpzvw/icRSaRHDGKU6fIFwMpRlyY=' ajax.googleapis.com; style-src 'self' 'sha256-qpNRdyufyDvsBeG2MdymLvMR4rfMK9yD4T8W2azmx8M='; font-src 'self'; img-src 'self' radjaherlangga.rf.gd; connect-src https://api.ipgeolocation.io/");
 
 
 if (isset($_POST['pesannya'])) {
+
+  // ? Validasi
   if (isset($_COOKIE['csrf'])) {
     if ($_COOKIE['csrf'] != $_POST['CSRF']) {
       $valid = false;
@@ -11,9 +14,13 @@ if (isset($_POST['pesannya'])) {
   } else {
     $valid = false;
   }
+  if (empty($_POST['pesannya'])){
+    $valid = false;
+  }
   if ($valid = false) {
     exit();
   }
+
   require("./env.php");
   $env = new env;
   $con = $env->connectdb();
@@ -53,14 +60,14 @@ setcookie (
     ]
   );
 ?>
-<html lang="en">
-
+<html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./style.css">
-  <link rel="shortcut icon" href="./image/favicon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="./storage/image/favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="./layouts/header.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <title>Radja Herlangga Web</title>
   <style>
@@ -89,6 +96,10 @@ setcookie (
       height: 100%;
     }
 
+    #link-github{
+      float: right;color:white;
+    }
+
     @media screen and (max-width: 800px) {
       .kiri-diri {
         width: 80%;
@@ -102,126 +113,8 @@ setcookie (
     }
   </style>
 </head>
-
+<?php include("./layouts/header.html"); ?>
 <body>
-  <style>
-    header {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      flex-direction: row;
-      height: 70px;
-      box-shadow: 0 4px 8px 0 rgba(250, 250, 250, 0.2),
-        0 6px 20px 0 rgba(255, 255, 255, 0.19);
-      border-radius: 0px 0px 8px 8px;
-    }
-
-    header ul {
-      list-style-type: none;
-    }
-
-    header ul li {
-      float: left;
-      margin-right: 20px;
-    }
-
-    header img {
-      height: 50px;
-      width: fit-content;
-    }
-
-    .menu {
-      height: 60px;
-    }
-
-    .link-menu {
-      text-decoration: none;
-      font-weight: bold;
-      color: #969595;
-    }
-
-    .link-menu:hover {
-      color: rgb(49, 49, 228);
-    }
-
-    .link-active {
-      color: rgb(224, 221, 221);
-      border-bottom: #3282b8 3px solid;
-      padding-bottom: 3px;
-    }
-
-    #kirim {
-      margin-top: 5px;
-      float: right;
-      font-family: inherit;
-      font-size: 18px;
-      background: royalblue;
-      color: white;
-      padding: 5px;
-      display: flex;
-      align-items: center;
-      border: none;
-      border-radius: 8px;
-      overflow: hidden;
-      transition: all 0.2s;
-      font-weight: lighter;
-    }
-
-    #kirim:hover {
-      cursor: pointer;
-    }
-
-    #kirim span {
-      display: block;
-      margin-left: 0.3em;
-      transition: all 0.3s ease-in-out;
-    }
-
-    #kirim svg {
-      display: block;
-      transform-origin: center center;
-      transition: transform 0.3s ease-in-out;
-    }
-
-    #kirim:hover .svg-wrapper {
-      animation: fly-1 0.6s ease-in-out infinite alternate;
-    }
-
-    #kirim:hover svg {
-      transform: translateX(1.2em) rotate(45deg) scale(1.1);
-    }
-
-    #kirim:hover span {
-      transform: translateX(5em);
-    }
-
-    #kirim:active {
-      transform: scale(0.95);
-    }
-
-    @keyframes fly-1 {
-      from {
-        transform: translateY(0.1em);
-      }
-
-      to {
-        transform: translateY(-0.1em);
-      }
-    }
-
-    .link-active:hover {
-      color: rgb(224, 221, 221);
-    }
-  </style>
-  <header>
-    <img src="./image/favicon.png" alt="logo rhcom">
-    <div class="menu">
-      <ul>
-        <li><a class="link-menu link-active" href="http://localhost/portofolio/">Home</a></li>
-        <!-- <li><a class="link-menu" href=".">Alamat</a></li> -->
-      </ul>
-    </div>
-  </header>
   <main>
     <div id="Perkenalan-diri">
       <div class="kanan-diri">
@@ -242,7 +135,7 @@ setcookie (
                 "></fecolormatrix>
           </filter>
         </svg>
-        <img src="./image/bg1.png" alt="gambar-diri-ku" id="diriku">
+        <img src="./storage/image/bg1.png" alt="gambar-diri-ku" id="diriku">
       </div>
       <div class="kiri-diri">
         <h2 id="my-name">M. Radja Herlangga</h2>
@@ -260,24 +153,24 @@ setcookie (
       <h2>Project saya</h2>
       <div class="datanya">
         <div class="card-ahli">
-          <img src="./image/ppdb-app.png" alt="gambar project RHcom">
+          <img src="./storage/image/ppdb-app.png" alt="gambar project RHcom">
           <h4>PPDB Sekolah</h4>
           <p>Aplikasi PPDB sekolah yang menggunakan Laravel, untuk memudahkan sekolah menerima murid baru jalur online
             dan mempercepat pengelolaan data</p>
         </div>
         <div class="card-ahli">
-          <!-- <img src="./image/gambar-project.jpg" alt="gambar project RHcom"> -->
+          <!-- <img src="./storage/image/gambar-project.jpg" alt="gambar project RHcom"> -->
           <h4>Aplikasi Chat</h4>
           <p>Aplikasi chat RHcom untuk mengchat biasa</p>
         </div>
         <div class="card-ahli">
-          <img src="./image/rhcom-rf-gd.png" alt="gambar project RHcom">
+          <img src="./storage/image/rhcom-rf-gd.png" alt="gambar project RHcom">
           <h4>Website Blog Mengoding</h4>
           <p>Website pertama yang saya buat untuk mempelajari CRUP pure PHP dengan kemanan yang sedang</p>
           <a href="http://rhcom.rf.gd/" target="_blank" rel="noopener noreferrer" class="btn">Buka Websitenya -&gt;</a>
         </div>
       </div>
-      <a href="https://github.com/RHcom007" style="float: right;color:white;">Lihat Selengkapnya disini &gt;&gt;</a>
+      <a href="https://github.com/RHcom007" id="link-github">Lihat Selengkapnya disini &gt;&gt;</a>
     </div>
     <h2 align="center">Kumpulan Keahlian</h2>
     <hr>
@@ -407,11 +300,11 @@ setcookie (
     <div class="social" id="social-media">
       <h4>Hubungiku dengan cara</h4>
       <div class="gambar-sosmed">
-        <a href="https://twitter.com/@RHcom007"><img src="./image/twitter.png" alt="icon twitter"></a>
-        <a href="https://facebook.com/RHcom007"><img src="./image/facebook.png" alt="icon facebook"></a>
-        <a href="https://instagram.com/RHcom007"><img src="./image/instagram.png" alt="icon twitter"></a>
-        <a href="https://tiktok.com/RHcom007"><img src="./image/tik-tok.png" alt="icon tiktok"></a>
-        <a href="https://github.com/RHcom007"><img src="./image/github.png" alt="icon github"></a>
+        <a href="https://twitter.com/@RHcom007"><img src="./storage/image/twitter.png" alt="icon twitter"></a>
+        <a href="https://facebook.com/RHcom007"><img src="./storage/image/facebook.png" alt="icon facebook"></a>
+        <a href="https://instagram.com/RHcom007"><img src="./storage/image/instagram.png" alt="icon twitter"></a>
+        <a href="https://tiktok.com/RHcom007"><img src="./storage/image/tik-tok.png" alt="icon tiktok"></a>
+        <a href="https://github.com/RHcom007"><img src="./storage/image/github.png" alt="icon github"></a>
       </div>
     </div>
   </main>
